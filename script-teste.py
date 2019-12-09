fbab62c77843078d3ff811d7933eb629268b3f05
BrunoPardo3596 <brunopardo3@gmail.com>
2019-12-09 18:44:08 -0300

    fix

--- script-teste.py
+++ script-teste.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
  
 import RPi.GPIO as gpio
+import uinput
 import time
  
  
@@ -13,8 +14,11 @@ gpio.setup(12, gpio.IN, pull_up_down = gpio.PUD_UP)
 while True:
   if gpio.input(12) == gpio.LOW:
     print("1")
+    with uinput.Device([uinput.KEY_E, uinput.KEY_H, uinput.KEY_L, uinput.KEY_O]) as device:
+        device.emit_click(uinput.KEY_E)
   else:
     print("0")
+  time.sleep(0.1)
  
 gpio.cleanup()
 exit()
