99c260294cc294b268ef71ec0485743e9450de02
BrunoPardo3596 <brunopardo3@gmail.com>
2019-12-09 18:40:24 -0300

    fix

--- script-teste.py
+++ script-teste.py
@@ -12,10 +12,10 @@ gpio.setup(12, gpio.IN, pull_up_down = gpio.PUD_UP)
  
 while True:
   if gpio.input(12) == gpio.LOW:
-    print(“Botão acionado”)
+    print("Botão acionado")
     break
   else:
-    printf(“Botão desligado”)
+    print("Botão desligado")
   time.sleep(1)
  
 gpio.cleanup()
