a618d202aa63d83e19a75be5688a87f6d8d5ad15
BrunoPardo3596 <brunopardo3@gmail.com>
2019-12-09 18:07:47 -0300

    fix

--- script.py
+++ script.py
@@ -34,7 +34,7 @@ def loop():
     # buttonXState = GPIO.input(buttonX)
     # buttonYState = GPIO.input(buttonY)
 
-    if  buttonUpState == True:
+    if (GPIO.input(buttonUp) == 1):
       print('yoooooooo')
       with uinput.Device([uinput.KEY_E, uinput.KEY_H, uinput.KEY_L, uinput.KEY_O]) as device:
         device.emit_click(uinput.KEY_E)
