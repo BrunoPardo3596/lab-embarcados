41223d90d7ef81b0f935d571bd2dc03d31e8d8dc
BrunoPardo3596 <brunopardo3@gmail.com>
2019-12-09 18:38:14 -0300

    fix

---
+++ script-teste.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+import RPi.GPIO as gpio
+import time
+ 
+ 
+gpio.setmode(gpio.BCM)
+ 
+# Com pull-up interno
+gpio.setup(12, gpio.IN, pull_up_down = gpio.PUD_UP)
+ 
+while True:
+ if gpio.input(12) == gpio.LOW:
+ print(“Botão acionado”)
+ break
+ else:
+ printf(“Botão desligado”)
+ 
+ time.sleep(1)
+ 
+gpio.cleanup()
+exit()
