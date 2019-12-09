aef30b2c457662eaa400c309e852afc8e0cd146b
BrunoPardo3596 <brunopardo3@gmail.com>
2019-12-09 18:51:51 -0300

    fix

--- script-teste.py
+++ script-teste.py
@@ -10,12 +10,14 @@ gpio.setmode(gpio.BCM)
  
 # Com pull-up interno
 gpio.setup(12, gpio.IN, pull_up_down = gpio.PUD_UP)
+
+events = (uinput.KEY_X, uinput.KEY_H, uinput.KEY_E, uinput.KEY_L, uinput.KEY_O)
+device = uinput.Device(events)
  
 while True:
   if gpio.input(12) == gpio.LOW:
     print("1")
-    with uinput.Device([uinput.KEY_E, uinput.KEY_H, uinput.KEY_L, uinput.KEY_O]) as device:
-      device.emit_click(uinput.KEY_E)
+    device.emit_click(uinput.KEY_E)
   else:
     print("0")
   time.sleep(0.1)
